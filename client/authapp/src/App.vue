d10f6c64701a77faa7f12274283b7e158054ab18
Khushbu B <khushabubelsare68@gmail.com>
2021-10-23 23:56:00 +0530

    Fixed issue toggle button not working in the mobile view.

--- client/authapp/src/App.vue
+++ client/authapp/src/App.vue
@@ -1,34 +1,34 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="nav" v-bind:class=" { 'navbarOpen': show }">
       <div class="container">
         <router-link to="/home" class="navbar-brand">Dashboard</router-link>
-        
+
         <button
           class="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          @click.stop="toggleNavbar()"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div
           class="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
+          id="navbarSupportedContent" v-bind:class="{ 'show': show }"
         >
           <ul class="navbar-nav">
             <li class="nav-item">
-               <router-link to="/groups" class="nav-link">Groups</router-link>
+              <router-link to="/groups" class="nav-link">Groups</router-link>
             </li>
             <li class="nav-item">
               <router-link to="/student" class="nav-link">Students</router-link>
-              
             </li>
-            <li class="nav-item active">
-              <a class="nav-link" @click="logUserOut"> Logout</a>
+            <li class="nav-item active logout-btn">
+              <a class="btn btn-danger" @click="logUserOut"> Logout</a>
             </li>
           </ul>
         </div>
@@ -40,17 +40,35 @@
 
 <script>
 export default {
+  data() {
+    return {
+      show:true
+    }
+  },
   methods: {
     logUserOut() {
       localStorage.removeItem("jwt");
       this.$router.push("/");
+    },
+    toggleNavbar(){
+      this.show = !this.show;
     }
   },
-}
+};
 </script>
 
 <style>
 .card {
   margin: 2em;
+}
+
+#nav a.router-link-exact-active {
+  color: rgb(1, 3, 136);
+  background: rgb(235, 230, 231);
+  border-radius: 0.5rem;
+}
+
+.logout-btn {
+  margin-left: 1em;
 }
 </style>
