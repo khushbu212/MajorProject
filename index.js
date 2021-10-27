9817a2c65e5e6235c46c4796c8728ab8e967d1ae
Khushbu B <khushabubelsare68@gmail.com>
2021-10-28 02:20:41 +0530

    Added dist folder serving the client.

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //configure body-parser ends here
 app.use(morgan("dev")); // configire morgan
-// define first route
-app.get("/", (req, res) => {
-  console.log("Hello MEVN Soldier");
-});
+
 const userRoutes = require("./api/user/route/user"); //bring in our user routes
 const studentRoutes = require("./api/student/route/student");
 const groupRoutes = require("./api/group/route/group");
@@ -36,6 +33,21 @@ app.use("/user", userRoutes);
 app.use("/student", studentRoutes);
 app.use("/group", groupRoutes);
 app.use("/email", emailRoutes);
+
+
+// define first route
+// app.get("/", (req, res) => {
+//   console.log("Hello MEVN Soldier");
+// });
+
+if(process.env.NODE_ENV === 'prodcution'){
+
+  app.use(express.static(__dirname + '/public/'));
+
+  app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'));
+
+}
+
 app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
 });
